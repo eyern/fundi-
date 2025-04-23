d8d5285b24be77a9f0c5c72382075d9ceacc3791
eyern <eyern254@gmail.com>
2025-04-23 14:39:19 +0300

    epheral volume

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static')]
 
 # Settings for Media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/app/data')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
